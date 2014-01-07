b297916801ec86edc0ddb5ab04fcfb1c2c1817f6
Finbar Sheahan <finbar.sheahan@wearefriday.com>
2014-01-07 17:06:54 +0000

    ostruct syntax compatible with old ruby

--- lib/shipping_forecast.rb
+++ lib/shipping_forecast.rb
@@ -49,20 +49,20 @@ class ShippingForecast
       # Warnings, if any
       warning = OpenStruct.new
       warning_detail = area.search(".warning-detail")
-      warning[:title]   = warning_detail.search("strong").text.gsub(':', '')
-      warning[:issued]  = warning_detail.search(".issued").text
-      warning[:summary] = warning_detail.search(".summary").text
+      warning.title   = warning_detail.search("strong").text.gsub(':', '')
+      warning.issued  = warning_detail.search(".issued").text
+      warning.summary = warning_detail.search(".summary").text
 
       # Build the hash
       area_hash = OpenStruct.new
       breakdown  = area.search("ul").children.search("span")
 
-      area_hash[:warning]    = warning
-      area_hash[:location]   = location
-      area_hash[:wind]       = breakdown[0].text
-      area_hash[:seas]       = breakdown[1].text
-      area_hash[:weather]    = breakdown[2].text
-      area_hash[:visibility] = breakdown[3].text
+      area_hash.warning    = warning
+      area_hash.location   = location
+      area_hash.wind       = breakdown[0].text
+      area_hash.seas       = breakdown[1].text
+      area_hash.weather    = breakdown[2].text
+      area_hash.visibility = breakdown[3].text
 
       locations[location] = area_hash
     end
