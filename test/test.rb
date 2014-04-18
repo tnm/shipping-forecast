a798f3e985cd7700254d9af540df266c1b33c8a8
Ted Nyman <ted@ted.io>
2014-04-18 10:05:08 -0700

    More tests for warning

--- test/test.rb
+++ test/test.rb
@@ -92,6 +92,24 @@ class ShippingForecastTest < Test::Unit::TestCase
       assert ShippingForecast[location][:warning].is_a?(Hash) if ShippingForecast[location][:warning]
     end
   end
+  
+  def test_warning_includes_title_if_warning
+    LOCATIONS.each do |location|
+      assert_not_nil_nor_empty ShippingForecast[location][:warning][:title] if ShippingForecast[location][:warning]
+    end
+  end
+
+  def test_warning_includes_issued_if_warning
+    LOCATIONS.each do |location|
+      assert_not_nil_nor_empty ShippingForecast[location][:warning][:issued] if ShippingForecast[location][:warning]
+    end
+  end
+
+  def test_warning_includes_summary_if_warning
+    LOCATIONS.each do |location|
+      assert_not_nil_nor_empty ShippingForecast[location][:warning][:summary] if ShippingForecast[location][:warning]
+    end
+  end
 
   def assert_not_nil_nor_empty(exp, msg=nil)
     msg = message(msg) { "<#{exp}> expected to not be nil or empty" }
